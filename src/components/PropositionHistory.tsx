d30a711265478e5050a102ce5da1fc8af5aa8fe5
EliasL94 <louhichielias94@gmail.com>
2026-09-10 11:49:07 +0200

    fix: resolve React DOM nesting bug causing first guess to not render

--- src/components/PropositionHistory.tsx
+++ src/components/PropositionHistory.tsx
@@ -20,16 +20,15 @@ export default function PropositionHistory({ propositions }: PropositionHistoryP
           title="Comment jouer à Communle ?"
           iconId="fr-icon-information-line"
         >
-          <div style={{ display: 'inline' }}>
+          <span style={{ display: 'inline' }}>
             Le but du jeu est de deviner la commune mystère du jour en un minimum d'essais.
             À chaque proposition, vous obtiendrez trois indices pour vous aider :
-          </div>
-          <ul style={{ display: 'block', marginTop: '0.5rem', marginBottom: '0.5rem' }}>
-            <li><strong>Distance :</strong> la distance à vol d'oiseau entre votre proposition et la commune mystère.</li>
-            <li><strong>Direction :</strong> la direction à suivre pour s'en rapprocher (Nord, Sud, Est, Ouest...).</li>
-            <li><strong>Population :</strong> si la commune mystère est plus ou moins peuplée que votre proposition.</li>
-          </ul>
-          <div style={{ display: 'inline' }} className="fr-mt-2v fr-mb-0"><em>Utilisez la barre de recherche ci-dessus pour faire votre première tentative !</em></div>
+            <br /><br />
+            <strong>- Distance :</strong> la distance à vol d'oiseau entre votre proposition et la commune mystère.<br />
+            <strong>- Direction :</strong> la direction à suivre pour s'en rapprocher (Nord, Sud, Est, Ouest...).<br />
+            <strong>- Population :</strong> si la commune mystère est plus ou moins peuplée que votre proposition.<br /><br />
+            <em>Utilisez la barre de recherche ci-dessus pour faire votre première tentative !</em>
+          </span>
         </CallOut>
       </div>
     );
